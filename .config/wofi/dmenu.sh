21666af78aace8f18aad47832f5a7575d36ef707
Alan Falconi <alanorbea@gmail.com>
2025-04-13 11:26:36 -0500

    Add Anki to wofi's display

--- .config/wofi/dmenu.sh
+++ .config/wofi/dmenu.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-apps="Spotify\nGoogle Chrome\nObsidian"
+apps="Spotify\nGoogle Chrome\nObsidian\nAnki"
 choice=$(echo -e "$apps" | wofi)
 
 case "$choice" in
     "Spotify") spotify ;;
     "Google Chrome") google-chrome-stable ;;
     "Obsidian") obsidian ;;
+    "Anki") anki ;;
 esac
